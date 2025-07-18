29bb0cedc16c827dce52915968cb0300839fe869
Ashish panwar <ashish05panwar@gmail.com>
2025-07-18 08:44:13 +0530

    Updated MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,8 @@
 @@ .. @@
  const MainLayout: React.FC = () => {
    return (
+   )
+ }
 -    <div className="flex flex-col min-h-screen bg-neutral-50">
 +    <div className="flex flex-col min-h-screen bg-neutral-900">
        <Header />
